c93b89ff8260a522c63d2eba27c7b6503b71b756
wangqiyang <wangqiyang@xiaomi.com>
2025-05-15 17:31:54 +0800

    Refactor robots function and update footer styles for improved accessibility and consistency
    - Changed the return structure of the `robots` function to align with `MetadataRoute.Robots` type.
    - Updated user agent rules to include an allow directive.
    - Modified footer styles to enhance text visibility and maintain consistency across light and dark themes.

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,12 @@
 import { baseUrl } from "@/app/sitemap";
+import { MetadataRoute } from "next";
 
-export default function robots() {
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
